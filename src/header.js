d13e8910dff8d6bde1e158142c995f0bc3eb2e0e
Arnav7107 <arnavverma345@gmail.com>
2023-06-04 23:10:21 +0530

    updated title

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import 'semantic-ui-css/semantic.min.css'
 
 const Header = () => {
     return(
-        <h1 style={{color:"red", paddingBottom:"20px"}}>Money Manager</h1>
+        <h1 style={{color:"red", paddingBottom:"20px"}}>Expense Tracker</h1>
     );
     
 }
