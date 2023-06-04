ed882463c5a9333a4fd1b911986bc110448e8e08
Arnav7107 <arnavverma345@gmail.com>
2023-06-04 17:32:45 +0530

    display_info

--- src/content.js
+++ src/content.js
@@ -49,15 +49,17 @@ const Content = () => {
 
          e.preventDefault();
         //  console.log(track_expense);
-         track_expense.push(amount);
-         setTrackExpense(track_expense);
+        //  track_expense.push({amount: amount, date: date, expense: expense});
+         setTrackExpense([...track_expense, {amount: amount, date: date, expense: expense}]);
+        //  setTrackExpense(track_expense);
          console.log(track_expense);
          setAmount('');
          setExpense('');
          setDate('');
          track_expense.forEach(x =>{
-            cal_total += x;
+            cal_total += x.amount;
          });
+         cal_total += amount;
          setTotalAmount(cal_total);
          setList(expense);
 
@@ -68,23 +70,25 @@ const Content = () => {
     return(
        
         <>
+        <div style={{justifyItems:'center'}}>
         <form>
+            
             <div>
                 <label>Enter the Amount: </label>
                 <input type="number" name = "amount"  value={amount} onChange={handle_amount} placeholder="Enter Amount" />
-                <h1>Amount = {amount}</h1>
+                {/* <h1>Amount = {amount}</h1> */}
             </div>
 
             <div style={{paddingTop:'20px'}}>
                 <label>Type of Expense: </label>
                 <input type="text" name = "amount"  value={expense} onChange={handle_expense} placeholder="Enter Reason" />
-                <h1>Type of Expense = {expense}</h1>
+                {/* <h1>Type of Expense = {expense}</h1> */}
             </div>
 
-            <div>
+            <div style={{paddingTop:'20px'}}>
                 <label>Enter the Date: </label>
                 <input type="date" name = "amount"  value={amount} onChange={handle_date} placeholder="Enter Date" />
-                <h1>Date = {date}</h1>
+                {/* <h1>Date = {date}</h1> */}
             </div>
             
             <div style={{paddingTop:'20px'}}>
@@ -93,9 +97,29 @@ const Content = () => {
             <p>{console.log(total_amount)}</p>
             
             <h1>Total Expense: {total_amount}</h1>
-            <p>Reason: {list}</p>
+            {/* <p>Reason: {list}</p> */}
+            <div>
+            {track_expense.map((expense) => (
+                <div>
+                    <p>
+                        <li>
+                        Reason:  {expense.expense}
+                        <br></br>
+                        Amount:  {expense.amount}
+                        <br></br>
+                        Date:   {expense.date}
+                        </li>
+                        
+                    </p>
+                </div>
+            ))}
+
+            </div>
+
             
+                        
         </form>
+        </div>
         </>
       
 
